f0c2c5c945a2121003ce2ec67bfde8f56578b299
khang <105849087+zocabulapo@users.noreply.github.com>
2025-05-01 23:34:36 +0700

    Update Base_Buff.h

--- Source/Fighter_Combat_Test1/Public/Effect/Base_Buff.h
+++ Source/Fighter_Combat_Test1/Public/Effect/Base_Buff.h
@@ -9,7 +9,7 @@
 UENUM(BlueprintType)
 enum class EBuffType : uint8
 {
-	None     UMETA(DisplayName = "None"), // Không có buff
+	None     UMETA(DisplayName = "None"), 
 	Burn     UMETA(DisplayName = "Burn"),
 	Poison   UMETA(DisplayName = "Poison"),
 	Freeze   UMETA(DisplayName = "Freeze"),
@@ -33,45 +33,34 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Đối tượng nhận Buff
 	UPROPERTY(BlueprintReadOnly, Category = "Buff")
 	AActor* TargetActor;
 
-	// Loại Buff (Burn, Poison, v.v.)
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Buff")
 	EBuffType BuffType;
 
-	// Thời gian hiệu lực của Buff
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Buff")
 	float Duration = 5.0f;
 
-	// Thêm thuộc tính Damage mỗi giây
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Buff")
 	float TickDamage = 5.0f;
 
-	// Thời gian đã trôi qua
 	float ElapsedTime = 0.0f;
 
-	// Gọi khi Buff được áp dụng
 	void InitializeBuff(AActor* Target, EBuffType Type);
 
-	// Hàm áp dụng Buff
 	void ApplyBuff();
 
-	// Hàm áp dụng Damage theo thời gian
 	void ApplyDamageOverTime();
 
-	// Hàm gỡ bỏ Buff
 	void RemoveBuff();
 
 private:
-	// Các hiệu ứng particle (Ví dụ: lửa cho Burn)
 	UPROPERTY(EditDefaultsOnly, Category = "Effects")
 	UParticleSystem* BurnEffect;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Effects")
 	UParticleSystem* PoisonEffect;
 
-	// Hiệu ứng Particle hiện tại
 	UParticleSystemComponent* ActiveEffect;
 };
