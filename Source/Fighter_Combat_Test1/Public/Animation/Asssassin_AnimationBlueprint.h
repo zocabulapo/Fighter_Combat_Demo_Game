d3d9ac5560f1de7dd85b9595e7b0a4cee815bbdc
khang <105849087+zocabulapo@users.noreply.github.com>
2025-05-01 23:32:53 +0700

    delete debug

--- Source/Fighter_Combat_Test1/Public/Animation/Asssassin_AnimationBlueprint.h
+++ Source/Fighter_Combat_Test1/Public/Animation/Asssassin_AnimationBlueprint.h
@@ -10,15 +10,12 @@ class FIGHTER_COMBAT_TEST1_API UAsssassin_AnimationBlueprint : public UAnimInsta
 	GENERATED_BODY()
 
 public:
-	// Hàm override để update biến mỗi frame
 	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
 
 protected:
-	// Tốc độ di chuyển
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
 	float Speed;
 
-	// Kiểm tra nhân vật đang bay trên không hay không
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
 	bool bIsInAir;
 };
